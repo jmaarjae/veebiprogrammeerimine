43437f017d344476d06938f1ed46ef52ed380bd3
Johanna Maarja Eller <jmaarjae@users.noreply.github.com>
2018-09-18 11:32:14 +0300

    Lisatud kataloogi sisu lugemine, massiivid ja tsükkel

--- tund_3/index.php
+++ tund_3/index.php
@@ -4,6 +4,11 @@
 $firstName = "Johanna Maarja";
 $lastName = "Eller";
 $dateToday = date("d.m.Y");
+$weekdayNow = date ("N");
+$weekdayNamesET = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
+//echo $weekdayNamesET[1];
+//var_dump ($weekdayNamesET);
+//echo $weekdayNow; 
 $hourNow = date("G");
 $partOfDay = "";
 if ($hourNow < 8){
@@ -15,6 +20,12 @@ if ($hourNow >= 8 and $hourNow < 16){
 if ($hourNow >= 16){
 	$partOfDay = "Loodetavasti vaba aeg";
 }
+
+$picNum = mt_rand (2, 43);
+//echo $picNum 
+$picURL = "http://www.cs.tlu.ee/~rinde/media/fotod/TLU_600x400/tlu_";
+$picEXT = ".jpg";
+$picFile = $picURL .$picNum .$picEXT;
 ?>
 
 
@@ -36,16 +47,20 @@ if ($hourNow >= 16){
   echo $firstName ." " .$lastName;
   ?>, IF18</h1>
   <p style="color:blue">See leht on loodud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames, ei pruugi parim välja näha ning kindlasti ei sisalda tõsiseltvõetavat sisu!</p>
+  
+  <p>Tundides tehtu: <a href="photo.php">photo.php</a></p>
+  
   <?php
-  echo "<p>Tänane kuupäev on: " .$dateToday .".</p> \n";
+  //echo "<p>Tänane kuupäev on: " .$dateToday .".</p> \n";
+  //echo "<p> Täna on " .$weekdayNow .", " .$dateToday .".<p> \n";
+  echo "<p> Täna on " .$weekdayNamesET[$weekdayNow - 1] .", " .$dateToday .".<p> \n";
   echo "<p> Lehe avamise hetkel oli kell " .date("H:i:s") .". Käes oli " .$partOfDay .".</p> \n";
   ?>
   <h2>Vittorio De Sica on öelnud:</h2>
   <p style="color:red">"Abielumehe ainus võimalus jätta endale viimane sõna on öelda naisele: "Sul on õigus.""</p>
   <!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
-  <img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">
-  
- 
-  <P>Mul on ka sõber, kes teeb oma <a href="../../~davierl" target="_blank">veebi</a></p>
+ <!--<img src="../../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+  <img src="<?php echo $picFile; ?>" alt="TLÜ Terra õppehoone">;
+  <P>Mul on ka sõber, kes teeb oma <a href="../../../~davierl" target="_blank">veebi</a></p>
 </body>
 </html>
